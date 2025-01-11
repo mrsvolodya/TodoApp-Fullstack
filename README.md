926fac8c39a0adc200fdbe1fd98a2626cfe799d1
mrsvolodya <vovamursky@gmail.com>
2025-01-11 20:37:53 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# React + TypeScript + Vite + Tailwind CSS
+
+npm run dev
+http://localhost:5173/
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <p className="text-6xl text-slate-700">adsfasdfsdf</p>;
+}
+
+export default App;
