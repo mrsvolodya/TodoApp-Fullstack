bb79c62ddb1386aec888924f98cac325f8f67801
mrsvolodya <vovamursky@gmail.com>
2025-01-12 15:11:03 +0200

    fix cypress test after connection C# backend

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -13,7 +13,6 @@ describe("cypress todo", () => {
 
   it("allow adding a new todo item", () => {
     cy.get('[data-cy="TodoTitleField"]').type(`${newTodo}{enter}`);
-
     cy.get('[data-cy="TodoItem"]').should("have.length.greaterThan", 0);
     cy.get('[data-cy="TodoItem"]').last().contains(newTodo);
   });
@@ -26,14 +25,36 @@ describe("cypress todo", () => {
   it("allows deleted element", () => {
     cy.get('[data-cy="TodoTitleField"]').type(`${newTodo}{enter}`);
 
-    cy.get('[data-cy="TodoDelete"]').click();
-    cy.get('[data-cy="TodoItem"]').should("not.exist");
+    cy.intercept("DELETE", "/api/todo/*").as("deleteTodo");
+
+    cy.get('[data-cy="TodoItem"]')
+      .last()
+      .within(() => {
+        cy.get('[data-cy="TodoDelete"]').click();
+      });
+
+    cy.wait("@deleteTodo");
+
+    cy.get('[data-cy="TodoItem"]').should("have.length.lessThan", 7);
   });
 
   it("verify checked todos", () => {
     cy.get('[data-cy="TodoTitleField"]').type(`${newTodo}{enter}`);
-    cy.get('[data-cy="TodoStatus"]').should("not.be.checked");
-    cy.get('[data-cy="TodoStatus"]').click();
-    cy.get('[data-cy="TodoStatus"]').should("be.checked")
+
+    cy.get('[data-cy="TodoItem"]')
+      .last()
+      .within(() => {
+        cy.get('[data-cy="TodoStatus"]').should("not.be.checked");
+        cy.get('[data-cy="TodoStatus"]').click();
+        cy.get('[data-cy="TodoStatus"]').should("be.checked");
+      });
+  });
+
+  after(() => {
+    cy.get('[data-cy="TodoDelete"]').each(($deleteButton) => {
+      cy.wrap($deleteButton).click();
+    });
+
+    cy.get('[data-cy="TodoItem"]').should("have.length", 0);
   });
 });
