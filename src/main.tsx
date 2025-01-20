43fab442a70716a4f8475f47beb673758d482ced
mrsvolodya <vovamursky@gmail.com>
2025-01-20 20:44:13 +0200

    fix-two-request

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import { App } from "./App.tsx";
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { TodoProvider } from "./components/context/TodoProvider.tsx";
 import "./index.css";
+import React from "react";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <TodoProvider>
-      <App />
-    </TodoProvider>
-  </StrictMode>
+  <TodoProvider>
+    <App />
+  </TodoProvider>
 );
